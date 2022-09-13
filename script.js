5bd32949d835159ec90b70c1a56015ba6f915f9f
Alfredo Fisher <alfredo.fisher44@gmail.com>
2022-09-13 15:43:34 -0300

    Js wiring
    Finished wiring most of the buttons with respective functions and event listeners (backspace, operators, clear). Storage and display values are working correctly. Added icon to backspace
    
    Started on the operate func logic, and equal button.

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 function operate(a, b, op) {
-    let operation = op;
 
+    let operation = op;
     switch (operation) {
-        case 'sum':
+        case '+':
             return a + b;
             break;
-        case 'sus':
+        case '-':
             return a - b;
             break;
-        case 'mult':
+        case 'x':
             return a * b;
             break;
-        case 'div':
+        case '/':
             return a / b;
             break;
     }
+
 };
 
 let display = document.getElementById('display');
+let storage = document.getElementById('console');
 let displayValue = 0;
 let storedValue = 0;
 
@@ -27,6 +29,36 @@ let numbers = Array.from(document.getElementsByClassName('num'));
 numbers.forEach((number) => {
     number.addEventListener('click', (e) => {
         displayValue += e.target.innerHTML;
-        display.textContent = displayValue;   
+        display.textContent = parseInt(displayValue);   
     })
+})
+
+let operators = Array.from(document.getElementsByClassName('op'));
+
+operators.forEach((op) => {
+    op.addEventListener('click', (e) => {
+        storedValue = parseInt(displayValue);
+        displayValue = 0;
+        display.textContent = displayValue;
+        storage.textContent = storedValue;
+    })
+})
+
+let clear = document.getElementById('clear');
+clear.addEventListener('click', () => {
+    displayValue = 0;
+    storedValue = 0;
+    display.textContent = 0;
+    storage.textContent = ' ';
+})
+
+let back = document.getElementById('back');
+back.addEventListener('click', () => {
+    displayValue = displayValue.slice(0, -1);
+    display.textContent = parseInt(displayValue);
+})
+
+let equal = document.getElementById('eq');
+equal.addEventListener('click', () => {
+    
 })

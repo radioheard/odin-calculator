bf3a5bc4d9fb1ce8751f08ab09e36f84b7b4d8b9
Alfredo Fisher <alfredo.fisher44@gmail.com>
2022-09-21 12:22:00 -0300

    Final commit
    Fixed operate function, it operates as expected. Operators carry operate function too. Added 0/0 interaction.
     All the buttons are working properly.
     Could possibly add . (decimal) operator in the future.

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
-function operate(a, b, op) {
-
-    let operation = op;
+let displayValue = 0;
+let storedValue = 0;
+let operation = null;
+function showImage() {
+    var rock = document.getElementById('rock');
+    rock.style.visibility = 'visible';
+    setTimeout(() => {
+        rock.classList.add("hidden");
+    }, 500);
+    setTimeout(() => {
+        rock.classList.remove("hidden");
+        rock.style.visibility = 'hidden';
+    }, 5000);
+}
+function operate(a, b) {
+if (a === 0 && b === 0 && operation === '/') {
+    display.textContent = 'Br0';
+    storage.textContent = 'Nah';
+    showImage();
+    
+} else {
     switch (operation) {
         case '+':
-            return a + b;
+            displayValue = a + b;
             break;
         case '-':
-            return a - b;
+            displayValue = a - b;
             break;
         case 'x':
-            return a * b;
+            displayValue = a * b;
             break;
         case '/':
-            return a / b;
+            displayValue = a / b;
             break;
     }
-
+    display.textContent = displayValue;
+    storage.textContent = `${a} ${operation} ${b}`;
+    storedValue = displayValue;
+}
 };
 
 let display = document.getElementById('display');
 let storage = document.getElementById('console');
-let displayValue = 0;
-let storedValue = 0;
+
 
 
 let numbers = Array.from(document.getElementsByClassName('num'));
@@ -37,10 +57,19 @@ let operators = Array.from(document.getElementsByClassName('op'));
 
 operators.forEach((op) => {
     op.addEventListener('click', (e) => {
-        storedValue = parseInt(displayValue);
-        displayValue = 0;
-        display.textContent = displayValue;
-        storage.textContent = storedValue;
+        if (operation == null) {
+            storedValue = parseInt(displayValue);
+            displayValue = 0;
+            display.textContent = displayValue;
+            storage.textContent = storedValue;
+            operation = e.target.innerHTML;
+        } else {
+            
+            let disp = parseInt(displayValue);
+            operate(storedValue, disp);
+            operation = e.target.innerHTML;
+            displayValue = 0;
+        }
     })
 })
 
@@ -48,17 +77,25 @@ let clear = document.getElementById('clear');
 clear.addEventListener('click', () => {
     displayValue = 0;
     storedValue = 0;
+    operation = null;
     display.textContent = 0;
     storage.textContent = ' ';
 })
 
 let back = document.getElementById('back');
 back.addEventListener('click', () => {
-    displayValue = displayValue.slice(0, -1);
-    display.textContent = parseInt(displayValue);
+    let str = displayValue.toString();
+    str = str.slice(0, -1);
+    displayValue = parseInt(str)
+    display.textContent = displayValue;
 })
 
 let equal = document.getElementById('eq');
 equal.addEventListener('click', () => {
-    
-})
+    let disp = parseInt(displayValue);
+    operate(storedValue, disp);
+    displayValue = 0;
+    storedValue = 0;
+    storage.textContent = ' ';
+    operation = null;
+});
